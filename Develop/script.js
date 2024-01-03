b53395ffe68e1009309689a7061ab23ae75f15d4
Elijah M <em@Elijahs-MBP.local>
2024-01-03 13:29:23 -0600

    Updated script.js file to execute code

--- Develop/script.js
+++ Develop/script.js
@@ -3,19 +3,20 @@ var generateBtn = document.querySelector("#generate");
 
 // !!!--Main Working function to generate password--!!!
 function generatePassword() {
-  
+   
   // Variables holding the values to generate password.
   var numbers = "0123456789".split('');
   var specialCharacters = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~".split('');
-  var alphabetLowercase = "abcdefghijklmnopqrstuvwxyz".split('');
-  var alphabetUppercase = alphabetLowercase.map((c) => c.toUpperCase());
+  var lowercaseCharacters = "abcdefghijklmnopqrstuvwxyz".split('');
+  var uppercaseCharacters = lowercaseCharacters.map((c) => c.toUpperCase());
   // var alphabetUppercase = alphabetLowercase.map(function(c) {c.toUpperCase()});
-  var allCharacters = alphabetLowercase.concat(alphabetUppercase, specialCharacters, numbers);
+  var allCharacters = lowercaseCharacters.concat(uppercaseCharacters, specialCharacters, numbers);
    // var min/max related to character length requirements (used in IF statement below).
    var min = 8;   
    var max = 129;
   // 'userChoice' is the variable for the user input.
   var userChoice;
+  // prompt retrieves value of user answer input
   userChoice = prompt("How many characters would you like the password to contain?");
   console.log(userChoice);
 
@@ -27,62 +28,43 @@ function generatePassword() {
     var lowerCase = confirm('Click OK to confirm including lowercase characters.');
     var upperCase = confirm('Click OK to confirm including uppercase characters.'); 
     var numeric = confirm('Click OK to confirm including numberic characters.'); 
-    var characterTypes = 0;
     var array = [];
 
     if (specialChars) {
-      characterTypes++;
+     array = array.concat(specialCharacters);
     } 
     if (lowerCase) {
-      characterTypes++;
-      array.push(alphabetLowercase);
+      array = array.concat(lowercaseCharacters);
     }
-    
     if (upperCase) {
-      characterTypes++;
+      array = array.concat(uppercaseCharacters);
     }
     if (numeric) {
-      characterTypes++;
+      array = array.concat(numbers);
     }
-    if (!characterTypes) {
-      alert('hey weird')
-      return;
-    } else if (characterTypes == 1) {
-      console.log(array);
-    }
-
-    console.log(characterTypes);
-
-
-    /* 
-    modulo 
-    User chooese yes for ONLY lower, upper, and numeric:
-    User selected 10 (means max of 8 from one single array)
-    math.random 1-8 (lowercase) Ex:5 lowercase
-    math.random 1-4 (uppercase Ex:3 uppercase
-    math.random 1-4 (uppercase Ex:3 uppercase.
-    */
-
-    if (specialChars) {
-      confirm
-    } else {
-    }
-  } else if (userChoice > max) {
-    alert(`Password length must be less than 129 characters.`);
-  } else {
-    return;
-  }
   
-// ---------------------------------------------------------------------------------------------------------------------------------------
-  // var index = Math.floor(Math.random() * specialCharacters.length);
-  // var computerChoice = allCharacters[index];
-}
 
+    console.log(array);
+    var output = [];
+
+// //--------------------------------------------------------------------------------------------------------------------------------------
+  for (var i = 0; i < userChoice; i++) {
+    var index = Math.floor(Math.random() * array.length);//1
+    var trueValue = array[index];//b
+    output.push(trueValue);
+  }
+
+  
+  
+  return output.join("");
+}
+}
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var password = generatePassword();//what is returned from genPassword = value of var password
   var passwordText = document.querySelector("#password");
 
+  // .value key word applies to <textarea> and <input> tags to grab or display a value via html
   passwordText.value = password;
 
 }
